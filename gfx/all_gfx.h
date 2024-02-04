706cb753a670e709450ced7303e2c14e6b65075f
aerglass <86316669+aerglass@users.noreply.github.com>
2024-02-04 17:50:04 -0400

    Add files via upload

---
+++ gfx/all_gfx.h
@@ -0,0 +1,53 @@
+// Graphics converted using PAGfx by Mollusk.
+
+#pragma once
+
+#include <PA_BgStruct.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+// Sprites:
+extern const unsigned char dar_Sprite[4608] _GFX_ALIGN; // Palette: dar_Pal
+extern const unsigned char crystal_Sprite[512] _GFX_ALIGN; // Palette: crystal_Pal
+extern const unsigned char cannon_Sprite[128] _GFX_ALIGN; // Palette: cannon_Pal
+extern const unsigned char xed_Sprite[256] _GFX_ALIGN; // Palette: xed_Pal
+extern const unsigned char cannon3index_Sprite[128] _GFX_ALIGN; // Palette: cannon3index_Pal
+extern const unsigned char cierra_Sprite[512] _GFX_ALIGN; // Palette: cierra_Pal
+extern const unsigned char lpca_Sprite[128] _GFX_ALIGN; // Palette: lpca_Pal
+extern const unsigned char cierrab_Sprite[512] _GFX_ALIGN; // Palette: cierrab_Pal
+extern const unsigned char rev01_Sprite[128] _GFX_ALIGN; // Palette: rev01_Pal
+extern const unsigned char josue_Sprite[256] _GFX_ALIGN; // Palette: josue_Pal
+
+// Backgrounds:
+extern const PA_BgStruct tileemap2;
+extern const PA_BgStruct tileemap2bg1;
+extern const PA_BgStruct tileemap2c;
+extern const PA_BgStruct tilemap3;
+extern const PA_BgStruct tilemap3c;
+extern const PA_BgStruct finalcutscene;
+extern const PA_BgStruct finalcutscenec;
+extern const PA_BgStruct lasersbg1;
+extern const PA_BgStruct lasersbg2;
+extern const PA_BgStruct lasersbg3;
+extern const PA_BgStruct lasersbg4;
+extern const PA_BgStruct actcutscene;
+extern const PA_BgStruct programmingdesign;
+extern const PA_BgStruct artdesigncreditsbg;
+
+// Palettes:
+extern const unsigned short dar_Pal[256] _GFX_ALIGN;
+extern const unsigned short crystal_Pal[256] _GFX_ALIGN;
+extern const unsigned short cannon_Pal[256] _GFX_ALIGN;
+extern const unsigned short xed_Pal[256] _GFX_ALIGN;
+extern const unsigned short cannon3index_Pal[256] _GFX_ALIGN;
+extern const unsigned short cierra_Pal[256] _GFX_ALIGN;
+extern const unsigned short lpca_Pal[256] _GFX_ALIGN;
+extern const unsigned short cierrab_Pal[256] _GFX_ALIGN;
+extern const unsigned short rev01_Pal[256] _GFX_ALIGN;
+extern const unsigned short josue_Pal[256] _GFX_ALIGN;
+
+#ifdef __cplusplus
+}
+#endif
